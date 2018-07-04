a1ff0712077439c90dada8d3221d8950ef6d86d1
AntonTimichev <enfild2008@yandex.ru>
2018-07-04 23:09:00 +0300

    Декоративыне элементы

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,9 @@
 var link = document.querySelector(".search-hotel");
 var popup = document.querySelector(".search-hotel-form");
+var arrival = document.querySelector("[name=arrival-date]");
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("form-show");
+    arrival.focus();
 });
